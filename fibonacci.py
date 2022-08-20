8324ba67a674be751a93e0829bb660858145ba86
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-08-20 11:47:11 +0530

    n=int(input()) a=0 b=1 c=0 for i in range(1,n+1):     print(a,end=' ')     c=a+b     a=b     b=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
 n=int(input())
-v=0
-k=1
-r=0
-for i in range(1,n+1,1):
-    print(v,end=' ')
-    r=v+k
-    v=k
-    k=r
+a=0
+b=1
+c=0
+for i in range(1,n+1):
+    print(a,end=' ')
+    c=a+b
+    a=b
+    b=c
