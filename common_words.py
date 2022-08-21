fb5d3b1fb1129dc6a346cdd3148776cf32bf9f49
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-08-21 22:43:50 +0530

    s1=input().lower().split(' ') s2=input().lower().split(' ') for i in s2:     if i in s1:         print(i,end=' ')

--- common_words.py
+++ common_words.py
@@ -1,5 +1,5 @@
-s1=(input().lower()).split(' ')
-s2=(input().lower()).split(' ')
+s1=input().lower().split(' ')
+s2=input().lower().split(' ')
 for i in s2:
     if i in s1:
         print(i,end=' ')
